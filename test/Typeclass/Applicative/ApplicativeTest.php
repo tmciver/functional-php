b52ea790d11eaf0f75e8508c7bbb1f0e88b0282d
Tim McIver <tim@timmciver.com>
2018-07-21 17:51:45 -0400

    Refactored `ApplicativeTest` to use the template pattern and added a sub- class `LinkedListApplicativeTest`.

--- test/Typeclass/Applicative/ApplicativeTest.php
+++ test/Typeclass/Applicative/ApplicativeTest.php
@@ -9,9 +9,12 @@ use TMciver\Functional\Either\Either;
 use TMciver\Functional\Validation\Validation;
 use TMciver\Functional\LinkedList\LinkedListFactory;
 
-class ApplicativeTest extends TestCase {
+abstract class ApplicativeTest extends TestCase {
 
   private $applicativesToTest;
+  private $applicative;
+
+  abstract function getApplicative();
 
   public function setUp() {
 
@@ -24,23 +27,31 @@ class ApplicativeTest extends TestCase {
     $this->applicativesToTest = [Maybe::fromValue(1),
 				 Either::fromValue(1),
 				 Validation::fromValue(1),
-				 (new LinkedListFactory())->pure(1),
 				 $maybeT];
   }
 
   public function testIdentity() {
 
     // test all applicatives
+    // TODO Remove once all applicatives have their own test subclass.
     foreach($this->applicativesToTest as $applicative) {
       $result = $applicative->pure('identity')->apply($applicative);
 
       $this->assertEquals($applicative, $result);
     }
+
+    $applicative = $this->getApplicative();
+    $ff = $applicative->pure('identity');
+    $fa = $applicative->pure("hello");
+    $result = $applicative->apply($ff, $fa);
+
+    $this->assertEquals($fa, $result);
   }
 
   public function testComposition() {
 
     // test all applicatives
+    // TODO Remove once all applicatives have their own test subclass.
     foreach($this->applicativesToTest as $applicative) {
       $compose = $applicative->pure('compose');
       $f = $applicative->pure('addOne');
@@ -51,11 +62,28 @@ class ApplicativeTest extends TestCase {
 
       $this->assertEquals($expectedResult, $result);
     }
+
+    $applicative = $this->getApplicative();
+    $compose = $applicative->pure('compose');
+    $ff = $applicative->pure('addOne');
+    $fg = $applicative->pure('addOne');
+    $fa = $applicative->pure(1);
+    $result = $applicative->apply(
+      $applicative->apply(
+        $applicative->apply($compose, $ff),
+        $fg
+      ),
+      $fa
+    );
+    $expectedResult  = $applicative->pure(3);
+
+    $this->assertEquals($expectedResult, $result);
   }
 
   public function testHomomorphism() {
 
     // test all applicatives
+    // TODO Remove once all applicatives have their own test subclass.
     foreach($this->applicativesToTest as $applicative) {
       $f = $applicative->pure('addOne');
       $x = $applicative->pure(1);
@@ -64,6 +92,14 @@ class ApplicativeTest extends TestCase {
 
       $this->assertEquals($exptectedResult, $result);
     }
+
+    $applicative = $this->getApplicative();
+    $ff = $applicative->pure('addOne');
+    $fa = $applicative->pure(1);
+    $result = $applicative->apply($ff, $fa);
+    $exptectedResult = $applicative->pure(addOne(1));
+
+    $this->assertEquals($exptectedResult, $result);
   }
 
   public function testInvoke() {
