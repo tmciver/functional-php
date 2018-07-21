2e37c571b30787b8b8ac9b6be97a251424ccab12
Tim McIver <tim@timmciver.com>
2018-07-21 19:48:14 -0400

    Account for calling `Either::pure` with a null value.

--- src/Either/EitherMonad.php
+++ src/Either/EitherMonad.php
@@ -7,6 +7,8 @@ use TMciver\Functional\Typeclass\BaseMonad;
 class EitherMonad extends BaseMonad {
 
   public function pure($v) {
-    return new Right($v);
+    return is_null($v) ?
+      new Left("Error: called Either::pure with a null value. ") :
+      new Right($v);
   }
 }
