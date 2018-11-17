4004f5fdd38ce3b9ce2121501dc738f21219abbe
Tim McIver <tim@timmciver.com>
2018-11-17 13:18:03 -0500

    Added tests for RightFavoringEitherMonad.

---
+++ test/Typeclass/Monad/RightFavoringEitherMonadTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TMciver\Functional\Test\Typeclass\Monad;
+
+use TMciver\Functional\Either\Monad\RightFavoringEitherMonad;
+
+class RightFavoringEitherMonadTest extends MonadTest {
+
+  private $eitherMonad;
+
+  public function setUp() {
+    $this->eitherMonad = new RightFavoringEitherMonad();
+  }
+
+  protected function getMonad() {
+    return $this->eitherMonad;
+  }
+
+  protected function getValue() {
+    return 1;
+  }
+
+  protected function getMonadicFunctionF() {
+    return function($i) { return $this->eitherMonad->pure($i + 1); };
+  }
+
+  protected function getMonadicFunctionG() {
+    return function($i) { return $this->eitherMonad->pure($i . ''); };
+  }
+}
